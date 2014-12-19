1b74a48ff6c2b447fa9f4fad570c035363b5622b
Jose Gisbert <jgisbert@umh.es>
2014-12-19 18:58:28 +0100

    Implementado método procesaNombres

--- src/main/scala/ManoInocente.scala
+++ src/main/scala/ManoInocente.scala
@@ -13,6 +13,34 @@ object ManoInocente {
     }
   }
 
+  /** Procesa nombres y devuelve un conjunto no ordenado de personas únicas.
+    *
+    * Por cada nombre genera, en función del patrón que adopta:
+    *
+    *  a. Si responde al patrón {{{([\p{L} ]*) y ([\p{L} ]*)}}} un conjunto de dos personas de forma que la pareja de una es la pareja de la otra y viceversa.
+    *  b. Un conjunto de una persona que no tiene pareja ([[scala.Some]]).
+    *
+    * @example
+    * {{{
+    * scala> ManoInocente.procesaNombres(Array("a", "b c y d", "e y f g", "h"))
+    * res0: scala.collection.immutable.Set[Persona] = Set(Persona(e,Some(f g)), Persona(f g,Some(e)), Persona(h,None), Persona(b c,Some(d)), Persona(a,None), Persona(d,Some(b c)))
+    * }}}
+    *
+    * @param nombres conjunto de nombres.
+    * @return conjunto no ordenado de personas únicas.
+    */
+  def procesaNombres(nombres: Array[String]) = {
+    val patrón = """([\p{L} ]*) y ([\p{L} ]*)""".r
+    val personas: Array[Set[Persona]] =
+      for (nombre <- nombres) yield {
+        nombre match {
+          case patrón(una, otra) => Set(Persona(una, Some(otra)), Persona(otra, Some(una)))
+          case soltera => Set(Persona(soltera, None))
+      }
+    }
+    personas.flatten.toSet
+  }
+
   /** Combina personas de acuerdo a las reglas del juego de amigo invisible.
     *
     * Toma dos conjuntos de personas y asigna aleatoriamente a cada persona del primer conjunto una persona del segundo con las siguientes restricciones (la condición de elegibilidad está implementada en [[Persona.puedeSerMiAmiga()]]):
