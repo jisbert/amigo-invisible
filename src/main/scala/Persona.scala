e1108bfef4e194280272858a57b8469361a7bc0f
Jose Gisbert <jgisbert@umh.es>
2014-12-19 10:09:11 +0100

    Mejorados los comentarios en Persona

--- src/main/scala/Persona.scala
+++ src/main/scala/Persona.scala
@@ -1,8 +1,15 @@
-/** Created by Jose Gisbert on 18/12/2014.
+/** Persona.
+  *
+  * @param nombre Nombre de la persona.
+  * @param pareja Opción que refleja el nombre de la pareja si es que la tiene.
+  *
+  * @author Jose Gisbert <jgisbert@umh.es>
+  * @version 0.1.0
+  * @since 18/12/2014
   */
 case class Persona(nombre: String, pareja: Option[String]) {
-  /**
-    * Determina si esta persona es pareja de la otra persona
+  /** Determina si esta persona es pareja de la otra persona. Si una de las personas no considera pareja a la otra el método determina que no son pareja.
+    *
     * @param ella La otra persona
     * @return Devuelve cierto si ambos tiene pareja y la pareja del uno es la del otro
     */
@@ -14,6 +21,9 @@ case class Persona(nombre: String, pareja: Option[String]) {
       } yield pareja == ella.nombre || suPareja == nombre
     somosPareja getOrElse false
   }
+
+  /** Determina si esta persona puede ser amiga de la otra persona. Si son la misma persona o son pareja no pueden ser amigas.
+    */
   val puedeSerMiAmiga: (Persona) => Boolean = esMiPareja _ andThen(!_)
 }
 
