05f7a9c0587a8f724aecf5910dce667e70cc414f
Jose Gisbert <jgisbert@umh.es>
2014-12-22 19:47:18 +0100

    Implementados casos de prueba
    Reformadas (Refactor) clases en las que se definen los casos de pruebas

--- src/test/scala/PruebaManoInocente.scala
+++ src/test/scala/PruebaManoInocente.scala
@@ -1,8 +1,32 @@
-import org.scalatest._
+import ManoInocente._
+import Persona.ordenarPorEmparejado
 
-class PruebaManoInocente extends FlatSpec with Matchers {
-  "ManoInocente" should "descomponer los nombres en solteros y parejas" in {
-    val args = Array("Juan", "Fran y Lucía", "María José y Tomás", "José Ramón", "Carol", "María del Mar", "Javi y Raquel", "Miguel Ángel y María Teresa", "Sergio y Carla", "Fátima y Luis Miguel", "Elsa", "Elena", "Sofía")
-    ManoInocente main args
+import scala.util.Random
+
+class PruebaManoInocente extends UnitTest {
+  // TODO: Implementar un generador de personas para las pruebas
+  val args = Array("Juan", "Fran y Lucía", "María José y Tomás", "José Ramón", "Carol", "María del Mar", "Javi y Raquel", "Miguel Ángel y María Teresa", "Sergio y Carla", "Fátima y Luis Miguel", "Elsa", "Elena", "Sofía")
+  val personas = procesaNombres(args)
+  val eligen = personas.toList.sorted(ordenarPorEmparejado).reverse
+  val elegidas = Random.shuffle(personas.toList)
+  val combinaciones = emparejaAmigas(eligen, elegidas, Map())
+  println(combinaciones mkString ",")
+
+  "ManoInocente" should "no dejar que dos o más personas elijan a la misma persona como amiga" in {
+    val amigas = combinaciones.values.toList
+    val duplicadas = amigas diff amigas.distinct
+    assert(duplicadas.isEmpty, ", hay personas que han sido elegidas dos veces")
+  }
+
+  it should "devolver un mapa en el que se apliquen las reglas del amigo invisible" in {
+    for {
+      (ella, amiga) <- combinaciones
+      elige <- eligen
+      if elige.nombre == ella
+      elegida <- elegidas
+      if elegida.nombre == amiga
+    } {
+      assert(elige puedeSerMiAmiga elegida, ", hay personas que no cumplen las reglas del amigo invisible")
+    }
   }
 }
